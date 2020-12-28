c8c74db14d321fbcd06c1e5f844dae83491484d4
Trevor Smith <trevorsmithbanjo@gmail.com>
2020-12-27 18:41:21 -0600

    Started on generate password function and stopped at conditional statements.

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// generatePassword function
+function generatePassword() {
+  var newPass = "";
+  var passLength = prompt("How many characters do you want?");
+  var lowerCase = confirm("Do you wan't lowercase letters?");
+  var upperCase = confirm("Do you want uppercase letters?");
+  var numeric = confirm("Do you want numbers?");
+  var special = confirm("Do you want special characters?");
+
+  if (passLength! >== 8 )
+}
