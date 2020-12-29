e7b24055bd6bbe0fe5549576db4b4955356a4cce
Trevor Smith <trevorsmithbanjo@gmail.com>
2020-12-29 12:53:02 -0600

    Got conditionl statements altering randomStr based on confirm messages to work properly.

--- script.js
+++ script.js
@@ -15,23 +15,52 @@ generateBtn.addEventListener("click", writePassword);
 
 // generatePassword function
 function generatePassword() {
+  // Declared variables
   var newPass = "";
+  var randomStr = "";
   var passLength = prompt("How many characters do you want?");
   var lowerCaseYes = confirm("Do you wan't lowercase letters?");
+  console.log(lowerCaseYes);
   var upperCaseYes = confirm("Do you want uppercase letters?");
+  console.log(upperCaseYes);
   var numericYes = confirm("Do you want numbers?");
+  console.log(numericYes);
   var specialYes = confirm("Do you want special characters?");
+  console.log(specialYes);
+
+  // Declared options object
   var options = {
     lowerCase: "abcdefghijklmnopqrstuvwxyz",
     upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     numeric: "0123456789",
     special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
   };
-  var randomStr = options.lowerCase + options.upperCase + options.numeric + options.special;
 
-  for (i = 0; i < passLength; i++) {
-    newPass += randomStr[Math.floor(Math.random() * randomStr.length)];
-    console.log(newPass);
+  // Test for passLength to be between 8 and 128
+  if (passLength > 7 && < 129) {
+    // Conditionals based on confirm messages
+    if (lowerCaseYes === true) {
+      randomStr += options.lowerCase;
+      console.log(randomStr);
+    }
+    if (upperCaseYes === true) {
+      randomStr += options.upperCase;
+      console.log(randomStr);
+    }
+    if (numericYes === true) {
+      randomStr += options.numeric;
+      console.log(randomStr);
+    }
+    if (specialYes === true) {
+      randomStr += options.special;
+      console.log(randomStr);
+    }
+
+    // For loop to select random characters based on conditionals
+    for (i = 0; i < passLength; i++) {
+      newPass += randomStr[Math.floor(Math.random() * randomStr.length)];
+      console.log(newPass);
+    }
+    return newPass;
   }
-  return newPass;
 }
