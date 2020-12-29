efbbc8ddc6cdb49699c463f3791a559504a00faa
Trevor Smith <trevorsmithbanjo@gmail.com>
2020-12-29 13:51:57 -0600

    Got the conditional test for a num between 8 and 128 to work.

--- script.js
+++ script.js
@@ -18,26 +18,33 @@ function generatePassword() {
   // Declared variables
   var newPass = "";
   var randomStr = "";
-  var passLength = prompt("How many characters do you want?");
-  var lowerCaseYes = confirm("Do you wan't lowercase letters?");
-  console.log(lowerCaseYes);
-  var upperCaseYes = confirm("Do you want uppercase letters?");
-  console.log(upperCaseYes);
-  var numericYes = confirm("Do you want numbers?");
-  console.log(numericYes);
-  var specialYes = confirm("Do you want special characters?");
-  console.log(specialYes);
-
-  // Declared options object
-  var options = {
-    lowerCase: "abcdefghijklmnopqrstuvwxyz",
-    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numeric: "0123456789",
-    special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-  };
+  var passLength = parseInt(prompt("How many characters do you want?"));
+  console.log(passLength);
+  console.log(typeof passLength);
 
   // Test for passLength to be between 8 and 128
-  if (passLength > 7 && < 129) {
+  if (passLength < 8 || passLength > 128) {
+    alert("Password length must be no less than 8 characters and no more than 128.");
+    return "Password length must be no less than 8 characters and no more than 128."
+  }
+  else {
+    var lowerCaseYes = confirm("Do you wan't lowercase letters?");
+    console.log(lowerCaseYes);
+    var upperCaseYes = confirm("Do you want uppercase letters?");
+    console.log(upperCaseYes);
+    var numericYes = confirm("Do you want numbers?");
+    console.log(numericYes);
+    var specialYes = confirm("Do you want special characters?");
+    console.log(specialYes);
+
+    // Declared options object
+    var options = {
+      lowerCase: "abcdefghijklmnopqrstuvwxyz",
+      upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      numeric: "0123456789",
+      special: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    };
+
     // Conditionals based on confirm messages
     if (lowerCaseYes === true) {
       randomStr += options.lowerCase;
